a2d1dc9956fcd35a997bcc29a8d0cb96087f788e
quentinaime <quentin.aime.dev@gmail.com>
2018-12-13 14:18:25 +0100

    setup completed

---
+++ client/src/views/Home/HomeContainer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Home from './Home';
+
+class HomeContainer extends Component {
+    state = {
+
+    } 
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <Home></Home>
+        );
+    }
+}
+
+export default HomeContainer
