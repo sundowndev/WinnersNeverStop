24f29a5c9f985dc479f79bc2aadca8af7f54a54b
Micka-dej <33249171+Micka-dej@users.noreply.github.com>
2019-01-17 11:26:58 +0100

    Page Propos

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomeContainer from '../../views/Home/HomeContainer';
 import IntroductionContainer from '../../views/Introduction/IntroductionContainer';
 import ChapitresContainer from '../../views/Chapitres/ChapitresContainer';
+import Propos from '../../views/APropos/Propos.js';
 import './App.css';
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
             <Route exact path='/' component={HomeContainer} />
             <Route exact path='/introduction' component={IntroductionContainer} />
             <Route exact path='/chapitres' component={ChapitresContainer} />
+            <Route exact path='/propos' component={Propos} />
           </div>
         </Router>
       </div>
