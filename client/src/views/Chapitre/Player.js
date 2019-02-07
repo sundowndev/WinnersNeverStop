afea16481e8b60d466d5c39690d774e344a4c49a
quentinaime <quentin.aime.dev@gmail.com>
2019-02-07 15:50:54 +0100

    tmp

--- client/src/views/Chapitre/Player.js
+++ client/src/views/Chapitre/Player.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Player = ({isPlayerVisible, iFrameWidth, iFrameHeight, hidePlayer}) => (
+const Player = ({isPlayerVisible, iFrameWidth, iFrameHeight, hidePlayer, videoUrl}) => (
     <section className="Player">
         {isPlayerVisible && 
             <div>
@@ -10,11 +10,13 @@ const Player = ({isPlayerVisible, iFrameWidth, iFrameHeight, hidePlayer}) => (
                 </div>
                 <div className="playerVideo">
                     <iframe
+                        id="player"
                         title="player"
                         width={iFrameWidth}
                         height={iFrameHeight}
                         sandbox="allow-same-origin allow-scripts"
                         src="https://peertube.social/videos/embed/43c97e8b-5762-4bcd-b6eb-3686b98e2e0c"
+                        // src={videoUrl}
                         frameBorder="0"
                         allowFullScreen
                     ></iframe>
@@ -25,6 +27,7 @@ const Player = ({isPlayerVisible, iFrameWidth, iFrameHeight, hidePlayer}) => (
 )
 
 Player.propTypes = {
+    videoUrl: PropTypes.string.isRequired,
     isPlayerVisible: PropTypes.bool.isRequired,
     iFrameWidth: PropTypes.string.isRequired,
     iFrameHeight: PropTypes.string.isRequired,
